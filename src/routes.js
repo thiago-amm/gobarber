746d32030a8b4596a18644b614fac112a526e5c6
thiago-amm <thiago-amm@outlook.com>
2019-07-24 20:46:30 -0300

    Concluída a implementação do envio de arquivos no diretório tmp/uploads via Multer.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -6,10 +8,15 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), (request, response) => {
+  return response.json({ ok: true });
+});
 
 export default routes;

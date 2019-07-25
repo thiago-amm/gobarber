7437b12eaf88c1b2216f66e9fe4172c9f80c37d0
thiago-amm <thiago-amm@outlook.com>
2019-07-25 07:39:36 -0300

    Associando cada objeto de User a um objeto File e persistindo no banco de dados.

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
-import File from '../app/models/File';
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
@@ -14,7 +14,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 

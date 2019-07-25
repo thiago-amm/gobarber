a9c2e207c9612ba81de381c5920673be4dbe6c91
thiago-amm <thiago-amm@outlook.com>
2019-07-25 07:04:10 -0300

    Alteração do nome dos parâmetros req e res para request e response respectivamente.

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -3,17 +3,17 @@ import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
 
-export default async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+export default async (request, response, next) => {
+  const authHeader = request.headers.authorization;
   if (!authHeader) {
-    return res.status(401).json({ error: 'Token não fornecido!' });
+    return response.status(401).json({ error: 'Token não fornecido!' });
   }
   const [, token] = authHeader.split(' ');
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    req.userId = decoded.id;
+    request.userId = decoded.id;
     return next();
   } catch (err) {
-    return res.status(401).json({ error: 'Token inválido!' });
+    return response.status(401).json({ error: 'Token inválido!' });
   }
 };
